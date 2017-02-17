ac5a7ed040015bee3e0d7c7e616b5d3bb3416512
A1 <internalmike@gmail.com>
2017-02-17 23:29:56 +0300

    ui.glade

--- src/Solid/Gui/Native.hs
+++ src/Solid/Gui/Native.hs
@@ -1,13 +1,20 @@
 module Solid.Gui.Native where
 #include <haskell>
+import Paths_solid
 
 solid :: IO ()
 solid = do
   void initGUI
-  window <- windowNew
-  void $ on window deleteEvent $ do
-    lift mainQuit
-    return True
+  b <- builderNew
+  ui <- getDataFileName "ui.glade"
+  builderAddFromFile b ui
+  window <- builderGetObject b castToWindow ("applicationwindow" :: S.Text)
+  void $ on window deleteEvent $ tryEvent $ lift mainQuit
   set window [windowTitle := ("Solid" :: S.Text)]
+  d <- builderGetObject b castToDrawingArea ("drawingarea" :: S.Text)
+  void $ on d draw $ do
+    setSourceRGB 0.7 0.7 0.7
+    rectangle 0.0 0.0 20.0 20.0
+    fill
   widgetShowAll window
   mainGUI
