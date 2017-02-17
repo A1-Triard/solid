098cd0ae621527ab524032cd04185056d3536abc
A1 <internalmike@gmail.com>
2017-02-17 22:33:57 +0300

    window

--- src/Solid/Gui/Native.hs
+++ src/Solid/Gui/Native.hs
@@ -2,4 +2,12 @@ module Solid.Gui.Native where
 #include <haskell>
 
 solid :: IO ()
-solid = putStrLn "someFunc"
+solid = do
+  void initGUI
+  window <- windowNew
+  void $ on window deleteEvent $ do
+    lift mainQuit
+    return True
+  set window [windowTitle := ("Solid" :: S.Text)]
+  widgetShowAll window
+  mainGUI
