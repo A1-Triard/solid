36e646c8347fb757e5b39f3d024c3841b37e3761
A1 <internalmike@gmail.com>
2017-11-19 00:05:05 +0300

    license headers, other tuning

--- src/Solid/Gui.hs
+++ src/Solid/Gui.hs
@@ -1,5 +1,219 @@
+--
+-- Copyright 2017 Warlock <internalmike@gmail.com>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 module Solid.Gui
   ( solid
   ) where
 
-import Solid.Gui.Native
+#include <haskell>
+import Solid.Solver
+import Paths_solid
+
+drawBody :: V2 Double -> RigidBody -> Render ()
+drawBody c b = do
+  let v = L.rotate (bodyDirection b) (V3 20.0 0.0 0.0)
+  let v1 = bodyPosition b + v
+  let v2 = bodyPosition b + V3 (v ^._y) (-(v ^._x)) (v ^._z)
+  let v3 = bodyPosition b + V3 (-(v ^._x)) (-(v ^._y)) (v ^._z)
+  let v4 = bodyPosition b + V3 (-(v ^._y)) (v ^._x) (v ^._z)
+  setSourceRGB (bodyColor b ^._x) (bodyColor b ^._y) (bodyColor b ^._z)
+  setLineWidth 1.0
+  moveTo (c ^._x + v1 ^._x) (c ^._y - v1 ^._y)
+  lineTo (c ^._x + v3 ^._x) (c ^._y - v3 ^._y)
+  lineTo (c ^._x + v4 ^._x) (c ^._y - v4 ^._y)
+  lineTo (c ^._x + v2 ^._x) (c ^._y - v2 ^._y)
+  lineTo (c ^._x + v1 ^._x) (c ^._y - v1 ^._y)
+  fill
+
+drawSpring :: V2 Double -> Vector RigidBody -> Spring -> Render ()
+drawSpring c b spring = do
+  let i1 = springBodyIndex1 spring
+  let i2 = springBodyIndex2 spring
+  let b1 = fromMaybe (error "") $ b !? i1
+  let b2 = fromMaybe (error "") $ b !? i2
+  let c1 = bodyPosition b1
+  let c2 = bodyPosition b2
+  let p1 = c1 + (L.rotate (bodyDirection b1) $ springBodyPoint1 spring)
+  let p2 = c2 + (L.rotate (bodyDirection b2) $ springBodyPoint2 spring)
+  setSourceRGB 0.8 0.8 0.8
+  setLineWidth 1.0
+  moveTo (c ^._x + p1 ^._x) (c ^._y - p1 ^._y)
+  lineTo (c ^._x + p2 ^._x) (c ^._y - p2 ^._y)
+  stroke
+  setSourceRGB (bodyColor b1 ^._x) (bodyColor b1 ^._y) (bodyColor b1 ^._z)
+  moveTo (c ^._x + c1 ^._x) (c ^._y - c1 ^._y)
+  lineTo (c ^._x + p1 ^._x) (c ^._y - p1 ^._y)
+  stroke
+  setSourceRGB (bodyColor b2 ^._x) (bodyColor b2 ^._y) (bodyColor b2 ^._z)
+  moveTo (c ^._x + c2 ^._x) (c ^._y - c2 ^._y)
+  lineTo (c ^._x + p2 ^._x) (c ^._y - p2 ^._y)
+  stroke
+
+drawSystem :: V2 Double -> System -> Render ()
+drawSystem c s = do
+  forM_ (bodies s) $ drawBody c
+  forM_ (springs s) $ drawSpring c (bodies s)
+  selectFontFace ("monospace" :: S.Text) FontSlantNormal FontWeightNormal
+  setFontSize 14
+  setSourceRGB 0.9 0.9 0.9
+  moveTo 10.0 20.0
+  showText $ "Time = " ++ show (round $ time s :: Int)
+  moveTo 10.0 40.0
+  showText $ "Energy = " ++ show (round (kineticEnergy s + potentialEnergy s) :: Int)
+
+testBodies :: Vector RigidBody
+testBodies = V.fromList
+  [ RigidBody
+      (V3 0.7 0.6 0.5)
+      1.0
+      (50.0 *^ (V3 (V3 1.0 0.0 0.0) (V3 0.0 1.0 0.0) (V3 0.0 0.0 1.0)))
+      (V3 (-150.0) 150.0 0.0)
+      (V3 0.0 0.0 0.0)
+      (L.axisAngle (V3 0.0 0.0 1.0) (0.5 * pi))
+      (V3 0.0 0.0 0.0)
+  , RigidBody
+      (V3 0.5 0.6 0.7)
+      1.0
+      (1e3 *^ (V3 (V3 1.0 0.0 0.0) (V3 0.0 1.0 0.0) (V3 0.0 0.0 1.0)))
+      (V3 150.0 (-150.0) 0.0)
+      (V3 0.0 0.0 0.0)
+      (L.axisAngle (V3 0.0 0.0 1.0) pi)
+      (V3 0.0 0.0 0.0)
+  ]
+
+nan :: Double
+nan = 0.0 / 0.0
+
+testSprings :: Vector Spring
+testSprings = V.fromList
+  [ Spring
+      0.2
+      25.0
+      0 (V3 50.0 (-50.0) 0.0)
+      1 (V3 50.0 50.0 0.0)
+      (V3 nan nan nan)
+      (V3 nan nan nan)
+      nan
+      (V3 nan nan nan)
+  ]
+
+currentSeconds :: IO Double
+currentSeconds = do
+  t <- getTime Monotonic
+  return $ fromIntegral (sec t) + fromIntegral (nsec t) * 1e-9
+
+data UI = UI
+  { timer :: !(IO Double)
+  , canvas :: !DrawingArea
+  , normalizeQ :: !CheckMenuItem
+  , keDiagram :: !Diagram
+  , peDiagram :: !Diagram
+  }
+
+queueFrame :: UI -> System -> IO ()
+queueFrame ui s = do
+  Rectangle _ _ w h <- widgetGetAllocation $ canvas ui
+  let y0 = fromIntegral h - 30.0
+  draw_id <- on (canvas ui) draw $ do
+    drawSystem (V2 (fromIntegral w / 2.0) (50.0 + (fromIntegral h - 300.0) / 2.0)) s
+    setLineWidth 1.0
+    setSourceRGB 0.8 0.8 0.8
+    moveTo 20.0 y0
+    lineTo 1040.0 y0
+    stroke
+    moveTo 30.0 (y0 + 10.0)
+    lineTo 30.0 (y0 - 200.0)
+    stroke
+    drawDiagram (V2 25.0 y0) (V3 0.3 0.6 0.6) (keDiagram ui)
+    drawDiagram (V2 25.0 y0) (V3 0.6 0.6 0.3) (peDiagram ui)
+  void $ timeoutAdd (frame ui s draw_id) $ round (1000.0 / fps)
+
+frame :: UI -> System -> ConnectId DrawingArea -> IO Bool
+frame ui s draw_id = do
+  t <- timer ui
+  nq <- checkMenuItemGetActive $ normalizeQ ui
+  signalDisconnect draw_id
+  let s1 = advance t nq s
+  let i = floor (20.0 * t)
+  let ui1 = ui { keDiagram = addPoint i (kineticEnergy s / 100.0) (keDiagram ui), peDiagram = addPoint i (potentialEnergy s / 100.0) (peDiagram ui) }
+  queueFrame ui1 s1
+  widgetQueueDraw $ canvas ui1
+  return False
+
+data Diagram = Diagram !Int !(Vector (Maybe Double))
+
+addPoint :: Int -> Double -> Diagram -> Diagram
+addPoint m v (Diagram ni d) =
+  go (m `mod` V.length d)
+  where
+    go i
+      | i == ni = Diagram ni d
+      | i > ni = Diagram i $ d // [(x, Just v) | x <- [(ni + 1) .. i]]
+      | otherwise = Diagram i $ d // [(x, Just v) | x <- [(ni + 1) .. (V.length d - 1)]] // [(x, Just v) | x <- [0 .. i]]
+
+drawDiagram :: V2 Double -> V3 Double -> Diagram -> Render ()
+drawDiagram (V2 x0 y0) (V3 cr cg cb) (Diagram ni d) = do
+  setLineWidth 1.0
+  setSourceRGB cr cg cb
+  evalStateT go (x0, 0, True)
+  stroke
+  where
+    go :: StateT (Double, Int, Bool) Render ()
+    go = V.forM_ d $ \my -> do
+      (x, i, s) <- get
+      case my of
+        Nothing -> put (x + 2.0, i + 1, s)
+        Just y -> do
+          if s
+            then lift $ moveTo x (y0 - y)
+            else return ()
+          if i == ni + 1
+            then do
+              lift stroke
+              put (x + 2.0, i + 1, True)
+            else do
+              lift $ lineTo x (y0 - y)
+              put (x + 2.0, i + 1, False)
+
+fps :: Double
+fps = 50.0
+
+dt :: Double
+dt = 0.0001
+
+solid :: IO ()
+solid = do
+  void initGUI
+  b <- builderNew
+  builderAddFromFile b =<< getDataFileName "ui.glade"
+  window <- builderGetObject b castToWindow ("applicationwindow" :: S.Text)
+  void $ on window deleteEvent $ tryEvent $ lift mainQuit
+  d <- builderGetObject b castToDrawingArea ("drawingarea" :: S.Text)
+  quit <- builderGetObject b castToMenuItem ("quititem" :: S.Text)
+  void $ on quit menuItemActivated mainQuit
+  n <- builderGetObject b castToCheckMenuItem ("normalize" :: S.Text)
+  t0 <- currentSeconds
+  let
+    ui = UI
+      { timer = (subtract t0) <$> currentSeconds
+      , canvas = d
+      , normalizeQ = n
+      , keDiagram = Diagram 0 (V.replicate 500 Nothing)
+      , peDiagram = Diagram 0 (V.replicate 500 Nothing)
+      }
+  queueFrame ui $ start dt testSprings testBodies
+  widgetShowAll window
+  mainGUI
--- src/Solid/Solver.hs
+++ src/Solid/Solver.hs
@@ -1,3 +1,19 @@
+--
+-- Copyright 2017 Warlock <internalmike@gmail.com>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 module Solid.Solver
   ( RigidBody (..)
   , Spring (..)
@@ -11,4 +27,157 @@ module Solid.Solver
   , advance
   ) where
 
-import Solid.Solver.Native
+#include <haskell>
+
+data RigidBody = RigidBody
+  { bodyColor :: !(V3 Double)
+  , bodyMass :: !Double
+  , bodySpTensorOfInertia :: !(M33 Double)
+  , bodyPosition :: !(V3 Double)
+  , bodyVelocity :: !(V3 Double)
+  , bodyDirection :: !(Quaternion Double)
+  , bodyAngularVelocity :: !(V3 Double)
+  }
+
+data Spring = Spring
+  { springStiffness :: !Double
+  , springBLength :: !Double
+  , springBodyIndex1 :: !Int
+  , springBodyPoint1 :: !(V3 Double)
+  , springBodyIndex2 :: !Int
+  , springBodyPoint2 :: !(V3 Double)
+  , springPoint1 :: !(V3 Double)
+  , springPoint2 :: !(V3 Double)
+  , springDelta :: !(Double)
+  , springDirection :: !(V3 Double)
+  }
+
+data System = System
+  { time :: !Double
+  , timeDelta :: !Double
+  , springs :: !(Vector Spring)
+  , bodies :: !(Vector RigidBody)
+  , kineticEnergy :: Double
+  , potentialEnergy :: Double
+  }
+
+data TorqueForce = TorqueForce
+  { torque :: !(V3 Double)
+  , force :: !(V3 Double)
+  }
+
+bodyKineticEnergy :: RigidBody -> Double
+bodyKineticEnergy b =
+  0.5 * bodyMass b *
+    ( L.quadrance (bodyVelocity b)
+    + (bodySpTensorOfInertia b !* bodyAngularVelocity b) `L.dot` bodyAngularVelocity b
+    )
+
+springPotentialEnergy :: Spring -> Double
+springPotentialEnergy s =
+  let d = springDelta s in
+  if d <= 0.0 then 0.0 else 0.5 * springStiffness s * d * d
+
+calcKineticEnergy :: Vector RigidBody -> Double
+calcKineticEnergy = V.foldl (\s -> (s +) . bodyKineticEnergy) 0.0
+
+calcPotentialEnergy :: Vector Spring -> Double
+calcPotentialEnergy = V.foldl (\s -> (s +) . springPotentialEnergy) 0.0
+
+bodyMomentum :: RigidBody -> V3 Double
+bodyMomentum b = bodyMass b *^ bodyVelocity b
+
+momentum :: Vector RigidBody -> V3 Double
+momentum = V.foldl (\s -> (s +) . bodyMomentum) (V3 0.0 0.0 0.0)
+
+bodyAngularMomentum :: RigidBody -> V3 Double
+bodyAngularMomentum b =
+  bodyMass b *^
+  ( L.cross (bodyPosition b) (bodyVelocity b)
+  + bodySpTensorOfInertia b !* bodyAngularVelocity b
+  )
+
+angularMomentum :: Vector RigidBody -> V3 Double
+angularMomentum = V.foldl (\s -> (s +) . bodyAngularMomentum) (V3 0.0 0.0 0.0)
+
+updateSpring :: Vector RigidBody -> Spring -> Spring
+updateSpring b spring =
+  let i1 = springBodyIndex1 spring in
+  let i2 = springBodyIndex2 spring in
+  let b1 = fromMaybe (error "") $ b !? i1 in
+  let b2 = fromMaybe (error "") $ b !? i2 in
+  let p1 = L.rotate (bodyDirection b1) $ springBodyPoint1 spring in
+  let p2 = L.rotate (bodyDirection b2) $ springBodyPoint2 spring in
+  let d = (bodyPosition b2 + p2) - (bodyPosition b1 + p1) in
+  spring { springPoint1 = p1, springPoint2 = p2, springDelta = L.norm d - springBLength spring, springDirection = L.signorm d }
+
+applySpring :: Vector TorqueForce -> Spring -> Vector TorqueForce
+applySpring forces spring =
+  let i1 = springBodyIndex1 spring in
+  let i2 = springBodyIndex2 spring in
+  let d = springDelta spring in
+  let sf = if d <= 0.0 then 0.0 else (springStiffness spring * d) *^ springDirection spring in
+  V.imap (update sf i1 i2) forces
+  where
+    update sf i1 i2 i (TorqueForce t f)
+      | i == i1 = TorqueForce (t - L.cross sf (springPoint1 spring)) (f + sf)
+      | i == i2 = TorqueForce (t + L.cross sf (springPoint2 spring)) (f - sf)
+      | otherwise = TorqueForce t f
+
+springForces :: Int -> Vector Spring -> Vector TorqueForce
+springForces m = V.foldl applySpring (V.replicate m (TorqueForce 0.0 0.0))
+
+advancePosition :: Double -> Bool -> Vector RigidBody -> Vector RigidBody
+advancePosition d n s =
+  V.map (\x -> x
+    { bodyPosition = bodyPosition x + d *^ bodyVelocity x
+    , bodyDirection = (if n then L.normalize else id) $ bodyDirection x + (0.5 * d) *^ (q $ L.inv44 (mB $ bodyDirection x) !* (mD !* bodyAngularVelocity x))
+    }) s
+  where
+    mD = V4 (V3 0.0 0.0 0.0) (V3 1.0 0.0 0.0) (V3 0.0 1.0 0.0) (V3 0.0 0.0 1.0)
+    mB (Quaternion v0 (V3 v1 v2 v3)) =
+      V4
+        (V4 v0 v1 v2 v3)
+        (V4 (-v1) v0 (-v3) v2)
+        (V4 (-v2) v3 v0 (-v1))
+        (V4 (-v3) (-v2) v1 v0)
+    q (V4 v0 v1 v2 v3) = Quaternion v0 (V3 v1 v2 v3)
+
+advanceVelocity :: Double -> Vector TorqueForce -> Vector RigidBody -> Vector RigidBody
+advanceVelocity d fs b =
+  V.zipWith (\x (TorqueForce t f) -> x
+    { bodyVelocity = bodyVelocity x + (d / bodyMass x) *^ f
+    , bodyAngularVelocity = bodyAngularVelocity x + d *^ ((L.inv33 $ bodySpTensorOfInertia x) !* ((t ^/ bodyMass x) ^-^ (mW $ bodyAngularVelocity x) !* (bodySpTensorOfInertia x !* bodyAngularVelocity x)))
+    }) b fs
+  where
+    mW (V3 x y z) =
+      V3
+        (V3 0.0 (-z) y)
+        (V3 z 0.0 (-x))
+        (V3 (-y) x 0.0)
+
+advanceStep :: Bool -> (Int -> Vector Spring -> Vector TorqueForce) -> System -> System
+advanceStep n forces s =
+  let b1 = advancePosition (timeDelta s) n $ bodies s in
+  let s1 = V.map (updateSpring b1) $ springs s in
+  let f = forces (V.length b1) s1 in
+  let b2 = advanceVelocity (timeDelta s) f b1 in
+  System (time s + timeDelta s) (timeDelta s) s1 b2 (calcKineticEnergy b2) (calcPotentialEnergy s1)
+
+advanceCore :: Double -> Bool -> (Int -> Vector Spring -> Vector TorqueForce) -> System -> System
+advanceCore t n forces s =
+  let steps = ceiling $ (t - time s) / (timeDelta s) in
+  let s1 = fromMaybe s $ listToMaybe $ drop steps $ iterate (advanceStep n forces) s in
+  s1 { kineticEnergy = calcKineticEnergy (bodies s1), potentialEnergy = calcPotentialEnergy (springs s1) }
+
+advance :: Double -> Bool -> System -> System
+advance t n = advanceCore t n springForces
+
+startCore :: Double -> (Int -> Vector Spring -> Vector TorqueForce) -> Vector Spring -> Vector RigidBody -> System
+startCore dt forces s b =
+  let s1 = V.map (updateSpring b) s in
+  let b1 = advanceVelocity (dt / 2.0) (forces (V.length b) s1) b in
+  System 0.0 dt s1 b1 (calcKineticEnergy b1) (calcPotentialEnergy s1)
+
+start :: Double -> Vector Spring -> Vector RigidBody -> System
+start dt = startCore dt springForces
